b8053b1fb07f461a35db55fa2f61b73162859202
Enric Lafont <lafonte@greenitglobe.com>
2018-02-06 19:15:28 +0100

    DO NOT MERGE, here only for informative purpouses
    When changed the raml file, the behaviour of the client changed, in old
    versions user_service.py had lot of code that is missing now and this changes
    the expected behaviour as capnp unmarshals only to json no to the type of the object

--- test/test_user.py
+++ test/test_user.py
@@ -2,10 +2,11 @@
 
 import unittest
 import random
+import logging
+import json
 
 from client import Client
 from client.user import user as UserClass
-
 
 class UserTests(unittest.TestCase):
 
@@ -19,15 +20,17 @@ class UserTests(unittest.TestCase):
         # Create a user
         rand = random.randint(1000, 10000)
         uc =  UserClass.create(addr='home', alias=['enric'], keyPub=['123'], uid=rand)
-        _, resp = self.client.user.updateUser(id=str(rand), data=uc)
-        if resp.status_code !=200: raise Exception("Unexpected response {}" % (resp.status_code) )
+        resp = self.client.user.updateUser(id=str(rand), data=uc)
+        assert resp.status_code == 200, "Unexpected response {}" % (resp.status_code) 
 
         # List users: validate if user can be found
-        users, resp = self.client.user.listUser()
-        if resp.status_code !=200: raise Exception("Unexpected response {}" % (resp.status_code) )
-        for u in users:
-            print(u.uid, rand)
-            if u.uid == rand:
+        users = self.client.user.listUser() #  returns text, older versions of this call returned json
+        # logging.info("list: %s ", users.content)
+        assert users.status_code == 200, "Unexpected response {}" % (users.status_code) 
+
+        for u in users.json(): # It returns a dictionary, in older version users was a list of user class
+            logging.info("testing %s and %s", rand,u['uid'] )
+            if u['uid'] == rand:
                 self.assertEqual(u.addr, 'home')
                 self.assertEqual(u.alias, ['enric'])
                 self.assertEqual(u.keyPub, ['123'])
@@ -38,7 +41,7 @@ class UserTests(unittest.TestCase):
 
         # Retrieve the user
         user, resp = self.client.user.getUser(id=str(rand))
-        if resp.status_code !=200: raise Exception("Unexpected response {}" % (resp.status_code) )
+        assert resp.status_code ==200, "Unexpected response {}" % (resp.status_code) 
         self.assertEqual(user.addr, 'home')
         self.assertEqual(user.alias, ['enric'])
         self.assertEqual(user.keyPub, ['123'])
@@ -52,17 +55,17 @@ class UserTests(unittest.TestCase):
         
         # Check the updated user
         newser, resp = self.client.user.getUser(id=str(rand))
-        if resp.status_code !=200: raise Exception("Unexpected response {}" % (resp.status_code) )
+        assert resp.status_code == 200 "Unexpected response {}" % (resp.status_code) 
         self.assertEqual(newser.addr, 'Work')
         self.assertEqual(newser.alias, ['ryd'])
         self.assertEqual(newser.keyPub, ['321'])
         self.assertEqual(newser.uid, rand)
         
         # Delete User
-        _, resp = self.client.user.deleteUser(id=str(rand))
-        if resp.status_code != 200: raise Exception("Unexpected response {}" % (resp.status_code) )
+        resp = self.client.user.deleteUser(id=str(rand))
+        assert resp.status_code == 200, "Unexpected response {}" % (resp.status_code) 
 
         _ , resp  = self.client.user.getUser(id=str(rand))
-        if resp.status_code != 403: raise Exception("Unexpected response {}" % (resp.status_code) )
+        assert resp.status_code == 404, "Unexpected response {}" % (resp.status_code) 
         
         
