c3d793f2289b15e552a21b5a4b86f5540dff6517
Enric Lafont <35031211+lafont-e@users.noreply.github.com>
2018-02-13 13:59:30 +0100

    Changes to help the merge (#14)
    * Delete a user code
    
    * Code for managing the users object
    
    * updated test_user code
    
    re-generate type to match capnp name + better testing
    
    * Directory managing and test done
    
    Some refactoring on User and Dir handlers
    
    * Added ACL handlers and test
    
    * BDomain Handlers and test
    
    * Bobject Handlers and test
    
    Changed Content-type for Content-Type
    
    * Added flask requirements

--- server/handlers/updateBobjectHandler.py
+++ server/handlers/updateBobjectHandler.py
@@ -1,6 +1,6 @@
 # THIS FILE IS SAFE TO EDIT. It will not be overwritten when rerunning go-raml.
 
-from flask import jsonify, request
+from flask import jsonify, request, current_app
 
 import json as JSON
 import jsonschema
@@ -8,19 +8,32 @@ from jsonschema import Draft4Validator
 
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-bobject_schema = JSON.load(open(dir_path + '/schema/bobject_schema.json'))
-bobject_schema_resolver = jsonschema.RefResolver('file://' + dir_path + '/schema/', bobject_schema)
-bobject_schema_validator = Draft4Validator(bobject_schema, resolver=bobject_schema_resolver)
+CLASS='bobject'
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+json_schema = JSON.load(open(dir_path + '/schema/' + CLASS + '_schema.json'))
+schema_resolver = jsonschema.RefResolver('file://' + dir_path + '/schema/', json_schema)
+schema_validator = Draft4Validator(json_schema, resolver=schema_resolver)
 
 def updateBobjectHandler(id):
+    redis = current_app.config['redis']
+    key = current_app.config['dbkeys'][CLASS]
+    capnp_schema = current_app.config['capnp'][CLASS]
 
     inputs = request.get_json()
 
     try:
-        bobject_schema_validator.validate(inputs)
+        schema_validator.validate(inputs)
     except jsonschema.ValidationError as e:
-        return jsonify(errors="bad request body"), 400
+        print(e.message)
+        return jsonify(errors="bad request body: %s" % e.message), 400
+        
 
-    return jsonify()
+    # create capnp object
+    capnp_data = capnp_schema.new_message(**inputs)
+
+    # save new object into kvs
+    redis.hset(key, str(capnp_data.uid), capnp_data.to_bytes_packed())
+
+    return jsonify(capnp_data.to_dict()), 200, {"Content-Type": "application/json"}
+
