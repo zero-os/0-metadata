70ecaf0eff706773e8d82d25f6cdb4650d5a4b55
Enric Lafont <lafonte@greenitglobe.com>
2018-02-13 17:04:40 +0100

    Benchmark script
    	Uses vegeta to make request to a 0-metadata server
    	When run, creates 4 files in the same directory with graph data

--- test/bench.sh
+++ test/bench.sh
@@ -7,51 +7,58 @@ usage() {
 }
 
 
-which vegeta
+a=$(which vegeta) 
 [ $? -ne 0 ] && echo "Vegeta Dependency missing" && exit 1
 
 [ "$1" = "" ] && usage
 
 url="$1"
 
-secs="50s"
+secs="10s"
+targets="./targets"
+maxrecords=999
 
-mkdir ./targets
-file="./targets/POST"
-echo -n "" > ${file}
-for i in $(seq 1 1000); do 
+
+echo -n "POST..."
+mkdir -p ${targets}
+postfile="${targets}/POST"
+getfile="${targets}/GET"
+delfile="${targets}/DEL"
+echo -n "" > ${postfile}
+echo -n "" > ${getfile}
+echo -n "" > ${delfile}
+for i in $(seq 1 ${maxrecords}); do 
     body="{\"uid\": $i,\"addr\": \"home\", \"alias\": [\"jsmith\"], \"keyPub\": [\"123\"]}"
     echo $body > "./targets/jsbody.${i}"
-    echo "POST ${url}/user/${i}" >> ${file}
-    echo "Content-Type: application/json" >> ${file}
-    echo "@./targets/jsbody.${i}" >> ${file}
-    echo >> ${file}
+    echo "POST ${url}/user/${i}" >> ${postfile}
+    echo "Content-Type: application/json" >> ${postfile}
+    echo "@./targets/jsbody.${i}" >> ${postfile}
+    echo >> ${postfile}
+    echo "GET ${url}/user/${i}" >> ${getfile}
+    echo "DELETE ${url}/user/${i}" >> ${delfile}
 done 
 
-vegeta attack -targets=${file} -duration=$secs | \
-vegeta report -reporter=plot > plot.POST.html
+vegeta attack -targets=${postfile} -duration=${secs} > result.POST
+cat result.POST | vegeta report -reporter=plot > plot.POST.html
 
-echo "GET"
-file="./targets/GET"
-echo -n "" > ${file}
-for i in $(seq 1 1000); do 
-    echo "GET ${url}/user/${i}" >> ${file}
-done
+echo -n "GET..."
+vegeta attack -targets=${getfile} -duration=${secs} > result.GET
+cat result.GET |vegeta report -reporter=plot > plot.GET.html
 
-vegeta attack -targets=${file} -duration=$secs| \
-vegeta report -reporter=plot > plot.GET.html
-
-echo "LIST"
+echo -n "LIST..."
 echo "GET ${url}/user" | \
-vegeta attack  -duration=$secs | \
-vegeta report -reporter=plot > plot.LIST.html
+vegeta attack  -duration=${secs} > result.LIST
+cat result.LIST | vegeta report -reporter=plot > plot.LIST.html
 
-echo "DELETE"
-file="./targets/DELETE"
-echo -n "" > ${file}
-for i in $(seq 1 1000); do 
-    echo "DELETE ${url}/user/${i}" 
+echo -n "DELETE..."
+vegeta attack -targets=${delfile} -duration=${secs} > result.DEL
+cat result.DEL | vegeta report -reporter=plot > plot.DEL.html
+
+
+echo -e "\r***** Summary Report *****    \r"
+for i in POST GET LIST DEL; do
+    echo -e "\n${i}"
+    cat result.${i} | vegeta report
 done
 
-vegeta attack -targets=${file} -duration=10s | \
-vegeta report -reporter=plot > plot.DEL.html
+rm -Rf ${targets}
